f0b400fc4b47aff42f3df9a1c1ac6dd79c0fee96
aryandevand <aryandevand19@gmail.com>
2025-02-21 02:09:00 +0530

    Background updated

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
 				<h1 className='text-3xl font-semibold text-center text-gray-300'>
 					Login
-					<span className='text-blue-500'> ChatApp</span>
+					<span className='text-blue-500'>  WChat</span>
 				</h1>
 
 				<form onSubmit={handleSubmit}>
@@ -63,7 +63,7 @@ const Login = () => {
 };
 export default Login;
 
-// STARTER CODE FOR THIS FILE
+
 // const Login = () => {
 // 	return (
 // 		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
